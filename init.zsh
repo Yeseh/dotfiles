279fbc39ed8ae09c99380b25d9121a50c816d7c4
Yeseh <48557715+Yeseh@users.noreply.github.com>
2021-02-02 18:09:48 +0100

    Add scripts to install node, go, and rust

--- init.zsh
+++ init.zsh
@@ -1,3 +1,5 @@
+mkdir ~/tempdl
+
 # Create symlinks
 ls -s ~/.dotfiles/.zshrc ~/.zshrc
 ls -s ~/.dotfiles/.gitconfig ~/.gitconfig
@@ -14,3 +16,14 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 curl -o- curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | zsh
 nvm install --lts
 nvm use --lts
+
+# Install Golang
+wget https://dl.google.com/go/go1.15.7.linux-amd64.tar.gz -P ~/tempdl
+tar -C ~/tempdl -xzf go1.15.7.linux-amd64.tar.gz 
+mv ~/tempdl/go /usr/local 
+export PATH=$PATH:/usr/local/go/bin
+
+# Install Rustlang
+curl https://sh.rustup.rs -sSf | sh -s -- -y --profile complete
+
+rm -rf ~/tempdl
